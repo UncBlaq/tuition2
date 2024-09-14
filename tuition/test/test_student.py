ee76cb4217b1a7adcee1cbf7ffad5f1cc06f4eb2
black <michealadereti484@gmail.com>
2024-09-14 07:42:30 -0700

    Updated the student sign_up and invalid sign_up test

--- tuition/test/test_student.py
+++ tuition/test/test_student.py
@@ -48,44 +48,41 @@ def setup_database():
 
 
 @pytest.mark.parametrize(
-        "full_name, email, password, DOB, field_of_interest, student_description", [("new student", "canvascoder01@gmail.com", "123pass", "2024-08-02", "string", "string")]
+        "full_name, email, phone_number, password, confirm_password, field_of_interest", [("new student", "canvascoder01@gmail.com", "123pass", "Password1#", "Password1#", "string")]
         )
-def test_signup(client, setup_database, full_name, email, password, DOB, field_of_interest, student_description):
+def test_signup(client, setup_database, full_name, email, phone_number, password, confirm_password, field_of_interest):
     response = client.post(
         "/student/signup",
         json={
             "full_name": full_name,
             "email": email,
-            "password": password,
-            "DOB": DOB,
-            "field_of_interest": field_of_interest,
-            "student_description": student_description
+            "phone_number": phone_number,
+            "field_of_interest": field_of_interest, 
+            "password" : password, 
+            "confirm_password": confirm_password
             }
     )
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json().get('full_name') == full_name
     assert response.json().get('email') == email
-    assert response.json().get('DOB') == DOB
     assert response.json().get('field_of_interest') == field_of_interest
-    assert response.json().get('student_description') == student_description
     assert response.json().get('is_verified') == False
 
 
 @pytest.mark.parametrize(
-        "full_name, email, password, DOB, field_of_interest, student_description", [("new student", "canvascoder01@gmail.com", "123pass", "2024-08-02", "string", "string")]
+        "full_name, email, phone_number, password, confirm_password, field_of_interest", [("new student", "canvascoder01@gmail.com", "123pass", "Password1#", "Password1#", "string")]
         )
-def test_invalid_email_sign_up(client, setup_database, full_name, email, password, DOB, field_of_interest, student_description):
+def test_invalid_email_sign_up(client, setup_database, full_name, email, phone_number, password, confirm_password, field_of_interest):
     #Sign up a user with existing details
     response = client.post(
         "/student/signup",
         json={
             "full_name": full_name,
             "email": email,
-            "password": password,
-            "DOB": DOB,
+            "phone_number": phone_number,
             "field_of_interest": field_of_interest,
-            "student_description": student_description
-            }
+            "password": password,
+        "confirm_password" : confirm_password     }
     )
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
@@ -93,7 +90,7 @@ def test_invalid_email_sign_up(client, setup_database, full_name, email, passwor
     
 
 
-# @pytest.mark.parametrize("username, password", [("newuser2@example.com", "123")])
-# def test_login(client, setup_database, username, email, password):
+# # @pytest.mark.parametrize("username, phone_number", [("newuser2@example.com", "123")])
+# # def test_login(client, setup_database, username, email, phone_number):
     
  
